4a132338dfd1f20918a6c5f976c7eb164ec42575
Andy <andy0ph@gmail.com>
2026-03-17 09:14:35 +0200

    added data into json and working get call

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-import exress from 'express';
+import express from 'express';
+import data from './src/data.json' with { type: 'json' };
 
-const app = exress();
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/data', (req, res) => {
+  res.json(data);
 });
 
 app.listen(3000, () => {
